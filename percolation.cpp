a522e14573698162ceb44c012d8a4164e5c43063
Anmol3299 <mittalanmol22@gmail.com>
2019-08-29 15:13:44 +0530

    Percolation v1.1
    1) Use better data types to accomodate bigger values.
    2) Some minor bug fixes and optimizations

--- percolation.cpp
+++ percolation.cpp
@@ -9,68 +9,58 @@ int main()
 {
   srand(time(0));
 
-  int N, total_els, iter = 0;
-  double probability;
+  unsigned int N;
+  unsigned long int total_els, iter = 0;
+  float probability;
   cout << "Enter size of matrix: ";
   cin >> N;
   total_els = N * N;
-  int rand_hash[total_els];
+  unsigned long int *rand_hash = new unsigned long int[total_els];
 
   // Added 2 because last 2 elements will be treated as virtual top and bottom respectively.
   WQuickUnionUF U1(total_els + 2);
-  int v_top = total_els;
-  int v_bottom = total_els + 1;
+  unsigned long int v_top = total_els;
+  unsigned long int v_bottom = total_els + 1;
 
-  for (int i = 0; i < total_els; i++)
+  for (unsigned long int i = 0; i < total_els; i++)
     rand_hash[i] = 0;
 
-  while (!U1.connected(v_top, v_bottom))
+  while (1)
   {
-    int r = rand() % total_els;
+    unsigned long int r = rand() % total_els;
     if (!rand_hash[r])
     {
-      // cout << r << endl;
       rand_hash[r] = 1;
       iter++;
 
       // Top connection
       if (r < N)
-      {
         U1.Union(v_top, r);
-        // cout << "v_top \n";
-      }
       else if (rand_hash[r - N])
-      {
         U1.Union(r - N, r);
-        // cout << "top_connect \n";
-      }
 
       // Botton connection
       if (r + N > total_els - 1)
-      {
         U1.Union(v_bottom, r);
-        // cout << "v_bottom \n";
-      }
       else if (rand_hash[r + N])
-      {
         U1.Union(r + N, r);
-        // cout << "bottom_connect \n";
-      }
+
       // Left connection
       if (r % N > 0 && rand_hash[r - 1] == 1)
-      {
         U1.Union(r - 1, r);
-        // cout << "Left connect \n";
-      }
+
       // Right connection
       if (r % N < N - 1 && rand_hash[r + 1] == 1)
-      {
         U1.Union(r + 1, r);
-        // cout << "Right connect \n";
-      }
+
+      probability = (double)iter / (double)total_els;
+      if (probability > 0.5 && U1.connected(v_top, v_bottom))
+        break;
     }
   }
-  probability = (float)iter / (float)total_els;
+  delete[] rand_hash;
+
+  probability = (double)iter / (double)total_els;
   cout << probability << endl;
 
   return 0;
--- weighted-quick-union.h
+++ weighted-quick-union.h
@@ -1,46 +1,47 @@
 class WQuickUnionUF
 {
-  int *id, *size;
-  int idSize;
+  unsigned long int *id, *size;
+  unsigned long int idSize;
 
-  int root(int i)
+  unsigned long int root(unsigned long int i)
   {
     while (i != id[i])
     {
       id[i] = id[id[i]];
       i = id[i];
     }
-
     return i;
   }
 
 public:
-  WQuickUnionUF(int N)
+  // Constructor
+  WQuickUnionUF(unsigned long int N)
   {
-    id = new int[N];
-    size = new int[N];
+    id = new unsigned long int[N];
+    size = new unsigned long int[N];
     idSize = N;
-    for (int i = 0; i < N; i++)
+    for (unsigned long int i = 0; i < N; i++)
     {
       id[i] = i;
       size[i] = 1;
     }
   }
+  // Destructor
   ~WQuickUnionUF()
   {
-    delete id;
-    delete size;
+    delete[] id;
+    delete[] size;
   }
 
-  bool connected(int p, int q)
+  bool connected(unsigned long int p, unsigned long int q)
   {
     return (root(p) == root(q));
   }
 
-  void Union(int p, int q)
+  void Union(unsigned long int p, unsigned long int q)
   {
-    int rootp = root(p);
-    int rootq = root(q);
+    unsigned long int rootp = root(p);
+    unsigned long int rootq = root(q);
 
     if (rootp == rootq)
       return;
